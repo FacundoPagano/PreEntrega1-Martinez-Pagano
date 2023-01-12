537937dfbe7b36f1835e2659a5c2cd07c4e2bcc0
Facundo Martinez Pagano <facupaganoo@gmail.com>
2023-01-12 20:17:56 -0300

    Entrega proyecto final

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,32 +29,32 @@ const Form = () => {
     }
     console.log(cartItem)
     return (
-        <>
-            <div onSubmit={Completar}>
-                {orderId ? <h2 className='text-center mt-20 text-6xl'>Gracias por elegirnos!! Su Id de compra es: {orderId}</h2>
-                    : <form className='flex flex-col m-5'>
-                        <h2 className='text-4xl mb-10'>Rellene el siguiente formulario para completar su compra!</h2>
-                        <input className='m-3' onChange={datosComprador} type="text" name="name" placeholder="Nombre" />
-                        <input className='m-3' onChange={datosComprador} type="tel" name="phone" placeholder="Telefono" />
-                        <input className='m-3' onChange={datosComprador} type="email" name="email" placeholder="Correo electrónico" />
-                        <button className='btn btn-primary' type="submit">Generar orden</button>
-                        <h2 className='text-center text-3xl'>Su total es de: ${getTotalPrice()}</h2>
-                    </form>}
-                <div className='flex -flex-col m-10'>
-                    {cartItem.map(cartOrder => {
-                        return (
-                            <div className='flex flex-col m-20'>
-                                <img src={cartOrder.image} alt="" />
-                                <h2 className='text-xl'>{cartOrder.name}</h2>
-                                <h3 className='text-xl'>${cartOrder.price}</h3>
-                                <h4 className='text-xl'>Cantidad: {cartOrder.cantidad}</h4>
-                                <h4 className='text-xl'>Subtotal: ${cartOrder.cantidad * cartOrder.price} </h4>
-                            </div>
-                        )
-                    })}
-                </div>
+    <>
+        <div onSubmit={Completar}>
+            {orderId ? <h2 className='text-center mt-20 text-6xl'>Gracias por elegirnos!! Su Id de compra es: {orderId}</h2>
+                : <form className='flex flex-col m-5'>
+                    <h2 className='text-4xl mb-10'>Rellene el siguiente formulario para completar su compra!</h2>
+                    <input className='m-3' onChange={datosComprador} type="text" name="name" placeholder="Nombre" />
+                    <input className='m-3' onChange={datosComprador} type="tel" name="phone" placeholder="Telefono" />
+                    <input className='m-3' onChange={datosComprador} type="email" name="email" placeholder="Correo electrónico" />
+                    <button className='btn btn-primary' type="submit">Generar orden</button>
+                    <h2 className='text-center text-3xl'>Su total es de: ${getTotalPrice()}</h2>
+                </form>}
+            <div className='flex -flex-col m-10'>
+                {cartItem.map(cartOrder => {
+                    return (
+                        <div className='flex flex-col m-20'>
+                            <img src={cartOrder.image} alt="" />
+                            <h2 className='text-xl'>{cartOrder.name}</h2>
+                            <h3 className='text-xl'>${cartOrder.price}</h3>
+                            <h4 className='text-xl'>Cantidad: {cartOrder.cantidad}</h4>
+                            <h4 className='text-xl'>Subtotal: ${cartOrder.cantidad * cartOrder.price} </h4>
+                        </div>
+                    )
+                })}
             </div>
-        </>
+        </div>
+    </>
     )
 }
 
